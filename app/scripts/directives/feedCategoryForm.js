dc3b6241f021cf66eb32f911e855d35d482c0fde
BigEngland <andrewgwardwell@gmail.com>
2015-06-01 15:55:54 -0400

    added a bunch

--- app/scripts/directives/feedCategoryForm.js
+++ app/scripts/directives/feedCategoryForm.js
@@ -15,11 +15,8 @@ angular
   		restrict: 'E',
   		templateUrl: './views/feedcategory.html',
   		controller: function($rootScope, $scope, $routeParams, myTunes){
-    	// 		$scope.artist = true;
-			  //   $scope.title = true;
-			  //   $scope.description = true;
-			  //   $scope.image = true;
-			  //   $scope.loved_count = true;
+
+    		$scope.catagories = [];
 
 		  	// $rootScope.displayField = function(field_name){
 		  	// 	if($scope[field_name]){
